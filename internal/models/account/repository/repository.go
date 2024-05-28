7077ea549f3fd1ac26484e28a492283fa9665e8d
NapatBourban <napatone123@gmail.com>
2024-05-28 14:59:47 +0700

    add get by id

--- internal/models/account/repository/repository.go
+++ internal/models/account/repository/repository.go
@@ -14,7 +14,7 @@ type AccountRepo struct {
 	db *sqlx.DB
 }
 
-func NewFileRepository(db *sqlx.DB) Account.AccountRepository {
+func NewFileRepository(db *sqlx.DB) account.AccountRepository {
 	return &AccountRepo{
 		db: db,
 	}
@@ -55,4 +55,16 @@ func (a *AccountRepo) GetAccountAdmin(ctx context.Context) (*entities.Account, e
 	}
 
 	return &admin, nil
+}
+
+func (a *AccountRepo) GetFileById(ctx context.Context, id *int64) (*entities.Account, error) {
+	var accountbyid entities.Account
+
+	err := a.db.GetContext(ctx, &accountbyid, repository_query.SQL_get_account, *id)
+	if err != nil {
+		log.Info(err)
+		return nil, err
+	}
+
+	return &accountbyid, nil
 }
