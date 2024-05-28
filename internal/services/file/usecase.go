2c44ee2fa8916ee3ee54c13559912a76280605cd
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-28 16:30:24 +0700

    feat(file-interface): Update file repository and usecase packages

--- internal/services/file/usecase.go
+++ internal/services/file/usecase.go
@@ -1,1 +1,14 @@
 package file
+
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/nutikuli/internProject_backend/internal/services/file/entities"
+)
+
+type FileUsecase interface {
+	OnUploadFile(c *fiber.Ctx, ctx context.Context, req *entities.FileUploaderReq) (*entities.File, int, error)
+	OnGetSourceFiles(c *fiber.Ctx, ctx context.Context) ([]*entities.File, int, error)
+	OnDeleteFileByIdAndEntity(c *fiber.Ctx, ctx context.Context, req *entities.FileEntityReq) (int, error)
+}
