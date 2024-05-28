b6ce24c9b0f34312f3a9af20517c677b6d0c2b71
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-05-28 18:05:01 +0700

    Create CreateAccountCustomer repository+query

--- internal/models/customer/repository/repository.go
+++ internal/models/customer/repository/repository.go
@@ -8,13 +8,14 @@ import (
 	"github.com/nutikuli/internProject_backend/internal/models/customer"
 	"github.com/nutikuli/internProject_backend/internal/models/customer/entities"
 	"github.com/nutikuli/internProject_backend/internal/models/customer/repository/repository_query"
+	"github.com/nutikuli/internProject_backend/pkg/utils"
 )
 
 type CustomerRepo struct {
 	db *sqlx.DB
 }
 
-func NewFileRepository(db *sqlx.DB) customer.CustomerRepository {
+func NewCustomerRepository(db *sqlx.DB) customer.CustomerRepository {
 	return &CustomerRepo{
 		db: db,
 	}
@@ -42,4 +43,30 @@ func (c *CustomerRepo) GetAccountCustomerById(ctx context.Context, id *int64) (*
 	}
 
 	return &accountbyid, nil
+}
+
+func (c *CustomerRepo) CreateAccountCustomer(ctx context.Context, user *entities.CustomerRegister) (*int64, error) {
+
+	args := utils.Array{
+		user.Id,
+		user.Name,
+		user.Password,
+		user.Phone,
+		user.Location,
+		user.Email,
+		user.Role,
+		user.Status,
+	}
+
+	log.Info(args)
+
+	res, err := c.db.ExecContext(ctx, repository_query.SQL_create_account_customer, args...)
+	if err != nil {
+		log.Info(err)
+		return nil, err
+	}
+
+	userId, _ := res.RowsAffected()
+
+	return &userId, nil
 }
