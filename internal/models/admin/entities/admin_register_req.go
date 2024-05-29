5dfd9b83c3178da9702ee095171d9008b2b43e26
Sunz098 <119925492+Sunz098@users.noreply.github.com>
2024-05-29 17:44:05 +0700

    add admin register

---
+++ internal/models/admin/entities/admin_register_req.go
@@ -0,0 +1,26 @@
+package entities
+
+type AdminRegisterReq struct {
+	Name         string `json:"name" from:"name" binding:"required"`
+	Password     string `json:"password" from:"password" binding:"required"`
+	Phone        string `json:"phone" from:"phone" binding:"required"`
+	Location     string `json:"location" from:"location" binding:"required"`
+	Email        string `json:"email" from:"email" binding:"required"`
+	Role         string `json:"role" from:"role" binding:"required"`
+	Status       bool   `json:"status" from:"status" binding:"required"`
+	PermissionID string `json:"permissino_id" from:"permissino_id" binding:"required"`
+} 
+
+
+
+func (a *AdminRegisterReq) GetEmail() *string {
+	return &a.Email
+}
+
+func (a * AdminRegisterReq) GetPassword() *string {
+	return &a.Password
+}
+
+func (a* AdminRegisterReq) GetRole() *string {
+	return &a.Role
+}
