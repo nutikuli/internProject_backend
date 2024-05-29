e7ad71b56e90419b995b14e35d2197607053cc83
Sunz098 <119925492+Sunz098@users.noreply.github.com>
2024-05-29 10:20:49 +0700

    add admin usecase

---
+++ internal/models/admin/usecase.go
@@ -0,0 +1,15 @@
+package admin
+
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	_adminDtos "github.com/nutikuli/internProject_backend/internal/models/admin/dtos"
+	_adminEntities "github.com/nutikuli/internProject_backend/internal/models/admin/entities"
+	_fileEntities "github.com/nutikuli/internProject_backend/internal/services/file/entities"
+)
+
+type AdminUseCase interface {
+	OnCreateAdminAccount(c *fiber.Ctx, ctx context.Context, adminDatReq *_adminEntities.AdminCreatedReq, filesDatReq []*_fileEntities.FileUploaderReq) (*_adminDtos.AdminFileRes, int, error)
+	OnGetAdminById(c *fiber.Ctx, ctx context.Context, adminId *int64) (*_adminDtos.AdminFileRes, int, error)
+}
