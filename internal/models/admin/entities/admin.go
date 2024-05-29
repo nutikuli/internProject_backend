27e1c30e66fba5045779496dd42b2e3131a511ee
Sunz098 <119925492+Sunz098@users.noreply.github.com>
2024-05-29 10:40:17 +0700

    add entitiesaccount in admin

--- internal/models/admin/entities/admin.go
+++ internal/models/admin/entities/admin.go
@@ -1,7 +1,11 @@
 package entities
 
+import(
+	"github.com/nutikuli/internProject_backend/internal/models/account/entities"
+)
 
 
 type Admin struct {
 	PermissionID  string `db:"permissionId"`
+	entities.Account
 }
