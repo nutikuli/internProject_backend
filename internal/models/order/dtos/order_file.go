7abb863e8183caed6f1b08e3a9227b5bdaedaa77
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-05-30 13:09:08 +0700

    order reference bank

--- internal/models/order/dtos/order_file.go
+++ internal/models/order/dtos/order_file.go
@@ -1,11 +1,13 @@
 package dtos
 
 import (
+	_bankDtos "github.com/nutikuli/internProject_backend/internal/models/bank/dtos"
 	_orderEntities "github.com/nutikuli/internProject_backend/internal/models/order/entities"
 	_fileEntities "github.com/nutikuli/internProject_backend/internal/services/file/entities"
 )
 
 type OrderWithFileRes struct {
-	OrderData *_orderEntities.Order `json:"order_data"`
-	FilesData []*_fileEntities.File `json:"files_data"`
+	OrderData *_orderEntities.Order    `json:"order_data"`
+	FilesData []*_fileEntities.File    `json:"files_data"`
+	BanksData []*_bankDtos.BankFileRes `json:"banks_data"`
 }
--- internal/models/order/usecase/usecase.go
+++ internal/models/order/usecase/usecase.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/nutikuli/internProject_backend/internal/models/bank"
+	_bankEntities "github.com/nutikuli/internProject_backend/internal/models/bank/entities"
 	"github.com/nutikuli/internProject_backend/internal/models/order"
 	_orderDtos "github.com/nutikuli/internProject_backend/internal/models/order/dtos"
 	_orderEntities "github.com/nutikuli/internProject_backend/internal/models/order/entities"
@@ -15,12 +17,14 @@ import (
 type orderUsecase struct {
 	orderRepo order.OrderRepository
 	fileRepo  file.FileRepository
+	bankRepo  bank.BankRepository
 }
 
-func NewStoreUsecase(orderRepo order.OrderRepository, fileRepo file.FileRepository) order.OrderUsecase {
+func NewStoreUsecase(orderRepo order.OrderRepository, fileRepo file.FileRepository, bankRepo bank.BankRepository) order.OrderUsecase {
 	return &orderUsecase{
 		orderRepo: orderRepo,
 		fileRepo:  fileRepo,
+		bankRepo:  bankRepo,
 	}
 }
 
@@ -44,9 +48,9 @@ func (s *orderUsecase) OnCreateOrder(c *fiber.Ctx, ctx context.Context, orderDat
 			EntityId:   *newOrderId,
 		}
 
-		_, fUrl, errOnCreatedFile := file.Base64toFile(c, true)
+		_, fUrl, status, errOnCreatedFile := file.EncodeBase64toFile(c, true)
 		if errOnCreatedFile != nil {
-			return nil, http.StatusConflict, errOnCreatedFile
+			return nil, status, errOnCreatedFile
 		}
 
 		fDatReq.FileData = *fUrl
@@ -72,8 +76,13 @@ func (s *orderUsecase) OnCreateOrder(c *fiber.Ctx, ctx context.Context, orderDat
 	}, http.StatusOK, nil
 }
 
-func (s *orderUsecase) OnGetOrderById(c *fiber.Ctx, ctx context.Context, Id *int64) (*_orderDtos.OrderWithFileRes, int, error) {
+func (s *orderUsecase) OnGetOrderById(ctx context.Context, Id *int64) (*_orderDtos.OrderWithFileRes, int, error) {
 	fileEntity := &_fileEntities.FileEntityReq{
+		EntityType: "Order",
+		EntityId:   *Id,
+	}
+
+	bankEntity := &_bankEntities.Bank{
 		EntityType: "Order",
 		EntityId:   *Id,
 	}
@@ -88,8 +97,14 @@ func (s *orderUsecase) OnGetOrderById(c *fiber.Ctx, ctx context.Context, Id *int
 		return nil, http.StatusInternalServerError, errOnGetOrder
 	}
 
+	banksRes, errOnGetBanks := s.bankRepo.GetBanksByStoreId(ctx, bankEntity)
+	if errOnGetBanks != nil {
+		return nil, http.StatusInternalServerError, errOnGetBanks
+	}
+
 	return &_orderDtos.OrderWithFileRes{
 		OrderData: orderRes,
 		FilesData: filesRes,
+		BanksData: banksRes,
 	}, http.StatusOK, nil
 }
