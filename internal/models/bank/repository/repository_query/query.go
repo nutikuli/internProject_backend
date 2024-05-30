b696378881562132dc30ca90f18bb5fd128d64a3
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-05-30 13:08:13 +0700

    fix : bank query get by store id

--- internal/models/bank/repository/repository_query/query.go
+++ internal/models/bank/repository/repository_query/query.go
@@ -3,9 +3,11 @@ package repository_query
 // get bank
 var SQL_get_bank = `SELECT id,name,accNumber,accName,status,avatarUrl,storeId FROM Bank;`
 
+// get bank by store id
+var SQL_get_bank_by_store_id = `SELECT id,name,accNumber,accName,status,avatarUrl,storeId FROM Bank WHERE storeId = ?;`
+
 // get bank by id
 var SQL_get_bank_by_id = `SELECT id,name,accNumber,accName,status,avatarUrl,storeId FROM Bank WHERE id = ?;`
 
 //insert bank
-
 var SQL_insert_bank = `INSERT INTO Bank (name,accNumber,accName,status,avatarUrl,storeId) VALUE(?,?,?,?,?,?);`
