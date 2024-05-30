5187783a25ff8aea438ff8ccf412bf83f4186706
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-30 14:47:07 +0700

    update(bank-entities): Update `Bank` struct to use `int64` for `Id` field

--- internal/models/bank/entities/bank.go
+++ internal/models/bank/entities/bank.go
@@ -1,7 +1,7 @@
 package entities
 
 type Bank struct {
-	Id         string `json:"id" db:"id"`
+	Id         int64  `json:"id" db:"id"`
 	Name       string `json:"name" db:"name"`
 	AccNumber  string `json:"acc_number" db:"accNumber"`
 	AccName    string `json:"acc_name" db:"accName"`
