64cb4fb45bdef0e61c87537c19f315f2a7036814
NapatBourban <napatone123@gmail.com>
2024-05-30 13:08:46 +0700

    usercase Resetpass

--- internal/models/account/entities/re_pass.go
+++ internal/models/account/entities/re_pass.go
@@ -1,9 +1,8 @@
 package entities
 
-
 type UpdatePass struct {
-	Id          int64  `json:"id" db:"id"`
-	Password    string `json:"password" db:"password"`
+	Id       int64  `json:"id" db:"id"`
+	Password string `json:"password" db:"password"`
 	// TODO: เพิ่ม file entites ที่เป็น array
-	Role      string    `json:"role" db:"role"`
+	Role string `json:"role" db:"role"`
 }
