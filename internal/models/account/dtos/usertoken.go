a39a46d321e76c610cd341625fa88149d442dccc
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-30 17:13:45 +0700

    feat(account-dtos): Add role field to UserToken struct

--- internal/models/account/dtos/usertoken.go
+++ internal/models/account/dtos/usertoken.go
@@ -3,6 +3,7 @@ package dtos
 type UserToken struct {
 	AccessToken string `json:"access_token"`
 	TokenType   string `json:"token_type"`
+	Role        string `json:"role"`
 	ExpiresIn   string `json:"expires_in"`
 	IssuedAt    string `json:"issued_at"`
 }
