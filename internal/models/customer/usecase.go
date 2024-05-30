c8de3d705b7a8649c19ecc03e333805a44a866d6
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-05-30 15:27:26 +0700

    fix : import usecase customer

--- internal/models/customer/usecase.go
+++ internal/models/customer/usecase.go
@@ -6,12 +6,11 @@ import (
 	"github.com/gofiber/fiber/v2"
 	_accDtos "github.com/nutikuli/internProject_backend/internal/models/account/dtos"
 	_customerDtos "github.com/nutikuli/internProject_backend/internal/models/customer/dtos"
-	"github.com/nutikuli/internProject_backend/internal/models/customer/entities"
 	_customerEntities "github.com/nutikuli/internProject_backend/internal/models/customer/entities"
 )
 
 type CustomerUsecase interface {
 	OnCreateCustomerAccount(c *fiber.Ctx, ctx context.Context, customerDatReq *_customerEntities.CustomerRegisterReq) (*_customerDtos.CustomerAccountFileRes, *_accDtos.UsersRegisteredRes, int, error)
 	OnGetCustomerById(ctx context.Context, Id *int64) (*_customerDtos.CustomerAccountFileRes, int, error)
-	OnUpdateCustomerById(ctx context.Context, userId int64, req *entities.CustomerUpdateReq) (int, error)
+	OnUpdateCustomerById(ctx context.Context, userId int64, req *_customerEntities.CustomerUpdateReq) (int, error)
 }
