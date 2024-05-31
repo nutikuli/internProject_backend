2838717d3a102519126fcfffd04b622f7908271b
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-05-31 11:29:51 +0700

    Repo:GetOrderProductsByOrderId

--- internal/models/order-product/repository.go
+++ internal/models/order-product/repository.go
@@ -8,4 +8,5 @@ import (
 
 type OrderProductRepository interface {
 	CreateOrder(ctx context.Context, order *entities.OrderProductCreateReq) (*int64, error)
+	GetOrderProductByOrderId(ctx context.Context, orderId *int64) ([]*entities.OrderProduct, error)
 }
