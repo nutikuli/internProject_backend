ab9459b2ea84fcc8bd2777969db33e1357fd7e5c
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-31 16:54:36 +0700

    refactor(order_product_entities): Remove unused field in OrderProductCreateReq

--- internal/models/order-product/entities/order_product_create.go
+++ internal/models/order-product/entities/order_product_create.go
@@ -1,7 +1,6 @@
 package entities
 
 type OrderProductCreateReq struct {
-	OrderId   int64 `json:"order_id" form:"order_id" binding:"required"`
 	ProductId int64 `json:"product_id" form:"product_id" binding:"required"`
 	Quantity  int64 `json:"ord_quantity" form:"order_id" binding:"required"`
 }
