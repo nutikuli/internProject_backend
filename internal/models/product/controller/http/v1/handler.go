16593ad59dba564374d54bd600794a04c0d3fc04
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-31 16:58:14 +0700

    feat(product-controller): Add HTTP handler for getting products by store ID
    This commit adds a new HTTP handler function, `GetProductsByStoreId`, to the product controller in the `v1` package. This handler retrieves products based on the provided store ID parameter. It validates the request parameter, performs the necessary database operations through the product use case, and returns the products as a JSON response.
    
    The addition of this handler enhances the functionality of the product controller by allowing clients to retrieve products specific to a particular store.
    
    Related user commits:
    - feat(product-repository): Add GetProductsByOrderId method to ProductRepository
    - feat(product-usecase): Add OnGetProductsByOrderId method to ProductUsecase
    - feat(product-dtos): Add HTTP handler for creating product categories
    - feat(product-category-repo): Update ProductCategoryRepository methods to use value types instead of pointers
    - feat(product-category-usecase): Update ProductCategoryUsecase interface and implementation

---
+++ internal/models/product/controller/http/v1/handler.go
@@ -0,0 +1,169 @@
+package v1
+
+import (
+	"context"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/nutikuli/internProject_backend/internal/models/product"
+	"github.com/nutikuli/internProject_backend/internal/models/product/dtos"
+)
+
+type prodConn struct {
+	prodUse product.ProductUsecase
+}
+
+func NewProductHandler(prodUse product.ProductUsecase) *prodConn {
+	return &prodConn{
+		prodUse: prodUse,
+	}
+}
+
+func (p *prodConn) GetProductsByStoreId(c *fiber.Ctx) error {
+	req, err := c.ParamsInt("store_id")
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":      fiber.StatusBadRequest,
+			"status_code": fiber.StatusBadRequest,
+			"message":     "error, invalid request store_id param",
+			"result":      nil,
+		})
+	}
+
+	req64 := int64(req)
+
+	var (
+		ctx, cancel = context.WithTimeout(c.Context(), time.Duration(30*time.Second))
+	)
+
+	defer cancel()
+
+	products, status, err := p.prodUse.OnGetProductsByStoreId(ctx, req64)
+	if err != nil {
+		return c.Status(status).JSON(fiber.Map{
+			"status":      status,
+			"status_code": status,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":      fiber.StatusOK,
+		"status_code": fiber.StatusOK,
+		"message":     nil,
+		"result":      products,
+	})
+}
+
+func (p *prodConn) GetProductById(c *fiber.Ctx) error {
+	req, err := c.ParamsInt("product_id")
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":      fiber.StatusBadRequest,
+			"status_code": fiber.StatusBadRequest,
+			"message":     "error, invalid request product_id param",
+			"result":      nil,
+		})
+	}
+
+	req64 := int64(req)
+
+	var (
+		ctx, cancel = context.WithTimeout(c.Context(), time.Duration(30*time.Second))
+	)
+
+	defer cancel()
+
+	product, status, err := p.prodUse.OnGetProductById(ctx, req64)
+	if err != nil {
+		return c.Status(status).JSON(fiber.Map{
+			"status":      status,
+			"status_code": status,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":      fiber.StatusOK,
+		"status_code": fiber.StatusOK,
+		"message":     nil,
+		"result":      product,
+	})
+}
+
+func (p *prodConn) CreateProduct(c *fiber.Ctx) error {
+	req := new(dtos.ProductFileReq)
+	if err := c.BodyParser(req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":      fiber.StatusBadRequest,
+			"status_code": fiber.StatusBadRequest,
+			"message":     "error, invalid request body",
+			"result":      nil,
+		})
+	}
+
+	var (
+		ctx, cancel = context.WithTimeout(c.Context(), time.Duration(30*time.Second))
+	)
+
+	defer cancel()
+
+	product, status, err := p.prodUse.OnCreateProduct(c, ctx, req.ProductData, req.FileData)
+	if err != nil {
+		return c.Status(status).JSON(fiber.Map{
+			"status":      status,
+			"status_code": status,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"status":      fiber.StatusCreated,
+		"status_code": fiber.StatusCreated,
+		"message":     nil,
+		"result":      product,
+	})
+}
+
+func (p *prodConn) DeleteProductById(c *fiber.Ctx) error {
+	req, err := c.ParamsInt("product_id")
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":      fiber.StatusBadRequest,
+			"status_code": fiber.StatusBadRequest,
+			"message":     "error, invalid request product_id param",
+			"result":      nil,
+		})
+	}
+
+	req64 := int64(req)
+
+	var (
+		ctx, cancel = context.WithTimeout(c.Context(), time.Duration(30*time.Second))
+	)
+
+	defer cancel()
+
+	status, err := p.prodUse.OnDeleteProductById(ctx, req64)
+	if err != nil {
+		return c.Status(status).JSON(fiber.Map{
+			"status":      status,
+			"status_code": status,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":      fiber.StatusOK,
+		"status_code": fiber.StatusOK,
+		"message":     nil,
+		"result":      nil,
+	})
+}
