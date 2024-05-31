481bd9c7160fda4163bf5222a4db97bae1aedff9
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-31 16:53:59 +0700

    feat(order-dtos): Update OrderBanksFilesRes struct to include product data

--- internal/models/order/dtos/order_file.go
+++ internal/models/order/dtos/order_file.go
@@ -2,14 +2,14 @@ package dtos
 
 import (
 	_bankDtos "github.com/nutikuli/internProject_backend/internal/models/bank/dtos"
-	_orderProductEntities "github.com/nutikuli/internProject_backend/internal/models/order-product/entities"
 	_orderEntities "github.com/nutikuli/internProject_backend/internal/models/order/entities"
+	_productDtos "github.com/nutikuli/internProject_backend/internal/models/product/dtos"
 	_fileEntities "github.com/nutikuli/internProject_backend/internal/services/file/entities"
 )
 
 type OrderBanksFilesRes struct {
-	OrderData         *_orderEntities.Order                 `json:"order_data"`
-	FilesData         []*_fileEntities.File                 `json:"files_data"`
-	BanksData         []*_bankDtos.BankFileRes              `json:"banks_data"`
-	OrdersProductData []*_orderProductEntities.OrderProduct `json:"order_product_data"`
+	OrderData         *_orderEntities.Order          `json:"order_data"`
+	FilesData         []*_fileEntities.File          `json:"files_data"`
+	BanksData         []*_bankDtos.BankFileRes       `json:"banks_data"`
+	OrdersProductData []*_productDtos.ProductFileRes `json:"product_data"`
 }
--- internal/models/order/dtos/order_file_req.go
+++ internal/models/order/dtos/order_file_req.go
@@ -1,11 +1,14 @@
 package dtos
 
 import (
+	_orderProductEntities "github.com/nutikuli/internProject_backend/internal/models/order-product/entities"
 	_orderEntities "github.com/nutikuli/internProject_backend/internal/models/order/entities"
 	_fileEntities "github.com/nutikuli/internProject_backend/internal/services/file/entities"
 )
 
-type OrderFileReq struct {
-	OrderData *_orderEntities.OrderCreate      `db:"order_data" form:"order_data" binding:"required"`
-	FilesData []*_fileEntities.FileUploaderReq `json:"files_data" form:"files_data" binding:"required"`
+type OrderFileBankIdOrderProductsReq struct {
+	BankId            int64                                          `json:"bank_id" form:"bank_id" binding:"required"`
+	FilesData         []*_fileEntities.FileUploaderReq               `json:"files_data" form:"files_data" binding:"required"`
+	OrderData         *_orderEntities.OrderCreate                    `db:"order_data" form:"order_data" binding:"required"`
+	OrderProductsData []*_orderProductEntities.OrderProductCreateReq `json:"order_product" form:"order_product" binding:"required"`
 }
