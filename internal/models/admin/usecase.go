30bb27e8ea38fe45294c77d04bdafb85005e3041
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-31 18:25:59 +0700

    refactor(admin-usecase): Update OnGetAdminById function signature to accept int64 ID

--- internal/models/admin/usecase.go
+++ internal/models/admin/usecase.go
@@ -4,16 +4,15 @@ import (
 	"context"
 
 	"github.com/gofiber/fiber/v2"
+	_accDtos "github.com/nutikuli/internProject_backend/internal/models/account/dtos"
 	_adminDtos "github.com/nutikuli/internProject_backend/internal/models/admin/dtos"
 	_adminEntities "github.com/nutikuli/internProject_backend/internal/models/admin/entities"
 	_fileEntities "github.com/nutikuli/internProject_backend/internal/services/file/entities"
-	_accDtos "github.com/nutikuli/internProject_backend/internal/models/account/dtos"
 )
 
 type AdminUseCase interface {
-	OnCreateAdminAccount(c *fiber.Ctx, ctx context.Context, adminDatReq *_adminEntities.AdminRegisterReq, filesDatReq []*_fileEntities.FileUploaderReq) (*_adminDtos.AdminFileRes,*_accDtos.UserToken, int, error)
-	OnGetAdminById( ctx context.Context, adminId *int64) (*_adminDtos.AdminFileRes, int, error)
+	OnCreateAdminAccount(c *fiber.Ctx, ctx context.Context, adminDatReq *_adminEntities.AdminRegisterReq, filesDatReq []*_fileEntities.FileUploaderReq) (*_adminDtos.AdminFileRes, *_accDtos.UserToken, int, error)
+	OnGetAdminById(ctx context.Context, adminId int64) (*_adminDtos.AdminFileRes, int, error)
 	OnUpdateUserById(ctx context.Context, Id int64, req *_adminEntities.AdminUpdateReq) (int, error)
-	AdminDeleted(ctx context.Context, Id int64) (int,error)
-	
+	AdminDeleted(ctx context.Context, Id int64) (int, error)
 }
