7112a09539a42bc90e26cd1863f6c7d618435bef
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-01 17:29:43 +0700

    feat(product-cate-controller): Update product-category handler to use more descriptive function name

--- internal/models/product-category/controller/http/v1/handler.go
+++ internal/models/product-category/controller/http/v1/handler.go
@@ -15,7 +15,7 @@ type prodCateConn struct {
 	prodCateUse product_category.ProductCategoryUsecase
 }
 
-func NewProductHandler(
+func NewProductCategoryHandler(
 	prodCateUse product_category.ProductCategoryUsecase,
 
 ) *prodCateConn {
@@ -164,6 +164,63 @@ func (p *prodCateConn) DeleteProductCategoryById(c *fiber.Ctx) error {
 	defer cancel()
 
 	status, err := p.prodCateUse.OnDeleteProductCategoryById(ctx, categoryIdInt64)
+	if err != nil {
+		return c.Status(status).JSON(fiber.Map{
+			"status":      http.StatusText(status),
+			"status_code": status,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+	}
+
+	return c.Status(status).JSON(fiber.Map{
+		"status":      http.StatusText(status),
+		"status_code": status,
+		"message":     nil,
+		"result":      nil,
+	})
+}
+
+func (p *prodCateConn) UpdateProductCategoryById(c *fiber.Ctx) error {
+	categoryId, err := c.ParamsInt("product_category_id")
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":      http.StatusText(http.StatusBadRequest),
+			"status_code": http.StatusBadRequest,
+			"message":     "error, invalid category id",
+			"result":      nil,
+		})
+	}
+
+	categoryIdInt64 := int64(categoryId)
+
+	req := new(entities.ProductCategoryCreatedReq)
+	if err := c.BodyParser(req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":      http.StatusText(http.StatusBadRequest),
+			"status_code": http.StatusBadRequest,
+			"message":     "error, invalid request body",
+			"result":      nil,
+		})
+	}
+
+	_, errOnValidate := utils.SchemaValidator(req)
+	if errOnValidate != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":      http.StatusText(http.StatusBadRequest),
+			"status_code": http.StatusBadRequest,
+			"message":     errOnValidate,
+			"result":      nil,
+		})
+	}
+
+	var (
+		ctx, cancel = context.WithTimeout(c.Context(), time.Duration(30*time.Second))
+	)
+
+	defer cancel()
+
+	status, err := p.prodCateUse.OnUpdateProductCategoryById(ctx, categoryIdInt64, req)
 	if err != nil {
 		return c.Status(status).JSON(fiber.Map{
 			"status":      http.StatusText(status),
