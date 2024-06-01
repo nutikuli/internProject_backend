2761d29edc02d8cf6683e565f55d6375e4211030
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-01 17:28:46 +0700

    feat(product-entities): Add ProductUpdateReq struct for updating product data

---
+++ internal/models/product/entities/product_update_req.go
@@ -0,0 +1,12 @@
+package entities
+
+type ProductUpdateReq struct {
+	Name          string  `json:"name" form:"name" validate:"required" binding:"required"`
+	Price         float64 `json:"price" form:"price" validate:"required" binding:"required"`
+	Stock         int64   `json:"stock" form:"stock" validate:"required" binding:"required"`
+	Detail        string  `json:"detail" form:"detail" validate:"required" binding:"required"`
+	Status        bool    `json:"status" form:"status" validate:"required" binding:"required"`
+	ProductAvatar string  `json:"product_avatar" form:"product_avatar" validate:"required" binding:"required"`
+	CategoryId    int64   `json:"category_id" form:"category_id" validate:"required" binding:"required"`
+	StoreId       int64   `json:"store_id" form:"store_id" validate:"required" binding:"required"`
+}
