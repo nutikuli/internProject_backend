b67d7b6a3ec7ca77dcaa01fccd8f9fef3e15521f
NapatBourban <napatone123@gmail.com>
2024-06-03 12:47:25 +0700

    pkg router acc

--- pkg/datasource/routes.go
+++ pkg/datasource/routes.go
@@ -8,6 +8,7 @@ import (
 	productCateRoute "github.com/nutikuli/internProject_backend/internal/models/product-category/controller/http/v1"
 	productRoute "github.com/nutikuli/internProject_backend/internal/models/product/controller/http/v1"
 	storeRoute "github.com/nutikuli/internProject_backend/internal/models/store/controller/http/v1"
+	accountRoute "github.com/nutikuli/internProject_backend/internal/models/account/controller/http/v1"
 )
 
 type RouteRepository interface {
@@ -25,5 +26,6 @@ func InitRoute(db *sqlx.DB, app *fiber.App) {
 	productCateRoute.UseProductCategoryRoute(db, apiEntry)
 	productRoute.UseProductRoute(db, apiEntry)
 	bankRoute.UseBankRoute(db, apiEntry)
+	accountRoute.UseAccountRoute(db,apiEntry)
 
 }
