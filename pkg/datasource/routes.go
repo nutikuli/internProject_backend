946aaa92d04e14d8a69907dff1bdce80c5aec6a7
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-06-03 12:53:01 +0700

    pkg/datasource/routes.go (customerController)

--- pkg/datasource/routes.go
+++ pkg/datasource/routes.go
@@ -3,12 +3,13 @@ package datasource
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/jmoiron/sqlx"
+	accountRoute "github.com/nutikuli/internProject_backend/internal/models/account/controller/http/v1"
 	bankRoute "github.com/nutikuli/internProject_backend/internal/models/bank/controller/http/v1"
+	customerRoute "github.com/nutikuli/internProject_backend/internal/models/customer/controller/http/v1"
 	orderRoute "github.com/nutikuli/internProject_backend/internal/models/order/controller/http/v1"
 	productCateRoute "github.com/nutikuli/internProject_backend/internal/models/product-category/controller/http/v1"
 	productRoute "github.com/nutikuli/internProject_backend/internal/models/product/controller/http/v1"
 	storeRoute "github.com/nutikuli/internProject_backend/internal/models/store/controller/http/v1"
-	accountRoute "github.com/nutikuli/internProject_backend/internal/models/account/controller/http/v1"
 )
 
 type RouteRepository interface {
@@ -26,6 +27,7 @@ func InitRoute(db *sqlx.DB, app *fiber.App) {
 	productCateRoute.UseProductCategoryRoute(db, apiEntry)
 	productRoute.UseProductRoute(db, apiEntry)
 	bankRoute.UseBankRoute(db, apiEntry)
-	accountRoute.UseAccountRoute(db,apiEntry)
+	accountRoute.UseAccountRoute(db, apiEntry)
+	customerRoute.UseCustomerRoute(db, apiEntry)
 
 }
