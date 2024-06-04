cfc960e98c243e0e0620a0186cb85c78af0fdfee
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-04 18:10:18 +0700

    refactor(order-product-model): Update OrderProductCreateReq field name in order_product_create.go
    The code changes update the field name `ord_quantity` to `Quantity` in the `OrderProductCreateReq` struct. This modification improves clarity and aligns with the naming conventions used in the codebase.

--- internal/models/order-product/entities/order_product_create.go
+++ internal/models/order-product/entities/order_product_create.go
@@ -2,5 +2,5 @@ package entities
 
 type OrderProductCreateReq struct {
 	ProductId int64 `json:"product_id" form:"product_id" binding:"required"`
-	Quantity  int64 `json:"ord_quantity" form:"order_id" binding:"required"`
+	Quantity  int64 `json:"ord_quantity" form:"ord_quantity" binding:"required"`
 }
--- internal/models/order-product/repository/repository_query/query.go
+++ internal/models/order-product/repository/repository_query/query.go
@@ -1,4 +1,4 @@
 package repository_query
 
-var SQL_create_order_product = `INSERT INTO OrderProduct(orderId,productId,quantity) VALUE=(?,?,?)`
+var SQL_create_order_product = `INSERT INTO OrderProduct(orderId,productId,quantity) VALUES(?,?,?)`
 var SQL_get_order_products_by_order_id = `SELECT * FROM OrderProduct WHERE orderId = ?`
