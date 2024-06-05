8396d259e02ddcbc973591d71f8db8107f48594e
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-05 10:23:46 +0700

    feat(bank-controller): Add logging to bank controller endpoints
    This commit adds logging functionality to the bank controller endpoints. It introduces a new logger middleware and includes logging statements in the CreateBank, GetBankById, GetBanksByStoreId, DeleteBankById, and UpdateBankById functions. The logging enhances the tracking and monitoring of bank-related actions.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

--- internal/models/bank/controller/http/v1/route.go
+++ internal/models/bank/controller/http/v1/route.go
@@ -6,7 +6,9 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/nutikuli/internProject_backend/internal/models/bank/repository"
 	"github.com/nutikuli/internProject_backend/internal/models/bank/usecase"
+	_logRepo "github.com/nutikuli/internProject_backend/internal/models/logdata/repository"
 	_fileRepo "github.com/nutikuli/internProject_backend/internal/services/file/repository"
+	"github.com/nutikuli/internProject_backend/pkg/middlewares"
 )
 
 func UseBankRoute(db *sqlx.DB, app fiber.Router) {
@@ -15,6 +17,9 @@ func UseBankRoute(db *sqlx.DB, app fiber.Router) {
 		return c.Next()
 	})
 
+	logRepo := _logRepo.NewLoggerRepository(db)
+	logger := middlewares.NewLogger(logRepo)
+
 	fileRepo := _fileRepo.NewFileRepository(db)
 
 	bankRepo := repository.NewBankRepository(db)
@@ -22,9 +27,54 @@ func UseBankRoute(db *sqlx.DB, app fiber.Router) {
 
 	bankConn := NewBankHandler(bankUse)
 
-	bankR.Post("/create-bank", bankConn.CreateBank)
-	bankR.Get("/get-bank-id/:bank_id", bankConn.GetBankById)
-	bankR.Get("/get-banks-by-store-id/:store_id", bankConn.GetBanksByStoreId)
-	bankR.Delete("/delete-bank-id/:bank_id", bankConn.DeleteBankById)
-	bankR.Patch("/update-bank-id/:bank_id", bankConn.UpdateBankById)
+	bankR.Post("/create-bank",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ธนาคาร",
+				Action: "เพิ่มข้อมูลธนาคาร",
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		bankConn.CreateBank)
+	bankR.Get("/get-bank-id/:bank_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ธนาคาร",
+				Action: "ดูข้อมูลธนาคาร หมายเลข " + c.Params("bank_id"),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		bankConn.GetBankById)
+	bankR.Get("/get-banks-by-store-id/:store_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ธนาคาร",
+				Action: "ดูข้อมูลธนาคาร ใน Store หมายเลข " + c.Params("store_id"),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		bankConn.GetBanksByStoreId)
+	bankR.Delete("/delete-bank-id/:bank_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ธนาคาร",
+				Action: "ลบข้อมูลธนาคาร หมายเลข " + c.Params("bank_id"),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		bankConn.DeleteBankById)
+	bankR.Patch("/update-bank-id/:bank_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ธนาคาร",
+				Action: "อัปเดตข้อมูลธนาคาร หมายเลข " + c.Params("bank_id"),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		bankConn.UpdateBankById)
 }
