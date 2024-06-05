aec7907c4c03febc3ca3bb337c1e6d0dbd5dfa0e
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-05 09:49:05 +0700

    feat(mdw): Update logger middleware to include username in log data

--- pkg/middlewares/logger_mdw.go
+++ pkg/middlewares/logger_mdw.go
@@ -1,32 +1,64 @@
 package middlewares
 
 import (
-	
+	"context"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/nutikuli/internProject_backend/internal/models/logdata/repository"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/nutikuli/internProject_backend/internal/models/logdata"
 	"github.com/nutikuli/internProject_backend/internal/models/logdata/entities"
 )
 
-type logger struct {
-	logRepo repository.LogRepo
+type LoggerAction struct {
+	Menu   string
+	Action string
 }
 
-func NewLogger(logRepo repository.LogRepo) *logger { 
+type logger struct {
+	logRepo logdata.LogRepository
+}
+
+func NewLogger(logRepo logdata.LogRepository) LoggerUsecase {
 	return &logger{logRepo: logRepo}
 }
 
-func (l *logger) LogRequest(c *fiber.Ctx) error {
+type LoggerUsecase interface {
+	LogRequest(c *fiber.Ctx, logAction *LoggerAction) error
+}
 
-    req := new(entities.LogGetReq)
-	if err := c.BodyParser(req); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"status":      http.StatusText(http.StatusBadRequest),
-			"status_code": http.StatusBadRequest,
-			"message":     "error, invalid request body",
+func (l *logger) LogRequest(c *fiber.Ctx, logAction *LoggerAction) error {
+
+	username := string(c.Request().Header.Peek("username"))
+	if username == "" {
+		username = "anonymous"
+	}
+
+	var (
+		ctx, cancel = context.WithTimeout(c.Context(), time.Duration(30*time.Second))
+	)
+
+	defer cancel()
+
+	logReq := &entities.LogCreateReq{
+		Fullname:      username,
+		MenuRequest:   logAction.Menu,
+		ActionRequest: logAction.Action,
+	}
+
+	_, err := l.logRepo.CreateLogData(ctx, logReq)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":      http.StatusText(http.StatusInternalServerError),
+			"status_code": http.StatusInternalServerError,
+			"message":     fmt.Errorf("Error while trying to create logAction %v, Error: %v", *logReq, err.Error()),
 			"result":      nil,
 		})
 	}
+
+	log.Info("LogAction: ", logAction, " by: ", username)
+
 	return c.Next()
 }
