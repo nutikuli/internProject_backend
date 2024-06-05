b5b6857c376cb7d664330c9b0936027ef20ba6d5
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-05 10:24:09 +0700

    feat(order-controller): Add logging to order controller endpoints
    This commit adds logging functionality to the order controller endpoints. It introduces a new logger middleware and includes logging statements in the CreateOrder, GetOrderById, GetOrdersByStoreId, GetOrdersByCustomerId, and UpdateOrderTransportDetailAndState functions. The logging enhances the tracking and monitoring of order-related actions.

--- internal/models/order/controller/http/v1/route.go
+++ internal/models/order/controller/http/v1/route.go
@@ -1,16 +1,20 @@
 package v1
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/jmoiron/sqlx"
 	_bankRepo "github.com/nutikuli/internProject_backend/internal/models/bank/repository"
 	_bankUse "github.com/nutikuli/internProject_backend/internal/models/bank/usecase"
+	_logRepo "github.com/nutikuli/internProject_backend/internal/models/logdata/repository"
 	_orderProdRepo "github.com/nutikuli/internProject_backend/internal/models/order-product/repository"
 	_orderProdUse "github.com/nutikuli/internProject_backend/internal/models/order-product/usecase"
 	"github.com/nutikuli/internProject_backend/internal/models/order/repository"
 	"github.com/nutikuli/internProject_backend/internal/models/order/usecase"
 	_prodUse "github.com/nutikuli/internProject_backend/internal/models/product/usecase"
+	"github.com/nutikuli/internProject_backend/pkg/middlewares"
 
 	_prodRepo "github.com/nutikuli/internProject_backend/internal/models/product/repository"
 
@@ -23,6 +27,9 @@ func UseOrderRoute(db *sqlx.DB, app fiber.Router) {
 		log.Infof("order : %v", c.Request().URI().String())
 		return c.Next()
 	})
+
+	logRepo := _logRepo.NewLoggerRepository(db)
+	logger := middlewares.NewLogger(logRepo)
 
 	fileRepo := _fileRepo.NewFileRepository(db)
 	fileUse := _fileUse.NewFileUsecase(fileRepo)
@@ -41,9 +48,54 @@ func UseOrderRoute(db *sqlx.DB, app fiber.Router) {
 
 	orderConn := NewOrderHandler(orderUse)
 
-	orderR.Post("/create-order", orderConn.CreateOrder)
-	orderR.Get("/get-store-order-by-id/:store_id/:order_id", orderConn.GetOrderById)
-	orderR.Get("/get-orders-by-store-id/:store_id", orderConn.GetOrdersByStoreId)
-	orderR.Get("/get-orders-by-customer-id/:customer_id", orderConn.GetOrdersByCustomerId)
-	orderR.Patch("/update-order-state/:order_id", orderConn.UpdateOrderTransportDetailAndState)
+	orderR.Post("/create-order",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "รายการสั่งซื้อ",
+				Action: "สร้างรายการสั่งซื้อ",
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		orderConn.CreateOrder)
+	orderR.Get("/get-store-order-by-id/:store_id/:order_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "รายการสั่งซื้อ",
+				Action: fmt.Sprintf("ดูข้อมูล Order ของหมายเลข %s ใน Store เลขหมายเลข  %s", c.Params("order_id"), c.Params("store_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		orderConn.GetOrderById)
+	orderR.Get("/get-orders-by-store-id/:store_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "รายการสั่งซื้อ",
+				Action: fmt.Sprintf("ดูข้อมูล Order ของ Store เลขหมายเลข  %s", c.Params("store_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		orderConn.GetOrdersByStoreId)
+	orderR.Get("/get-orders-by-customer-id/:customer_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "รายการสั่งซื้อ",
+				Action: fmt.Sprintf("ดูข้อมูล Order ของ Customer เลขหมายเลข  %s", c.Params("customer_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		orderConn.GetOrdersByCustomerId)
+	orderR.Patch("/update-order-state/:order_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "รายการสั่งซื้อ",
+				Action: fmt.Sprintf("อัปเดตสถานะ Order หมายเลข  %s", c.Params("order_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		orderConn.UpdateOrderTransportDetailAndState)
 }
