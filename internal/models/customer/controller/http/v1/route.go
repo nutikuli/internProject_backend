ebb9ed5c4f54c5a3385ffbcbb788436974778001
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-05 10:23:59 +0700

    feat(customer-controller): Add logging to customer controller endpoints
    This commit adds logging functionality to the customer controller endpoints. It introduces a new logger middleware and includes logging statements in the CreateCustomerAccount, GetAllCustomer, GetCustomerById, UpdateCustomerById, and DeletedCustomerByID functions. The logging enhances the tracking and monitoring of customer-related actions.

--- internal/models/customer/controller/http/v1/route.go
+++ internal/models/customer/controller/http/v1/route.go
@@ -8,6 +8,8 @@ import (
 	_accUse "github.com/nutikuli/internProject_backend/internal/models/account/usecase"
 	"github.com/nutikuli/internProject_backend/internal/models/customer/repository"
 	_customerUse "github.com/nutikuli/internProject_backend/internal/models/customer/usecase"
+	_logRepo "github.com/nutikuli/internProject_backend/internal/models/logdata/repository"
+	"github.com/nutikuli/internProject_backend/pkg/middlewares"
 )
 
 func UseCustomerRoute(db *sqlx.DB, app fiber.Router) {
@@ -15,6 +17,9 @@ func UseCustomerRoute(db *sqlx.DB, app fiber.Router) {
 		log.Infof("customer : %v", c.Request().URI().String())
 		return c.Next()
 	})
+
+	logRepo := _logRepo.NewLoggerRepository(db)
+	logger := middlewares.NewLogger(logRepo)
 
 	customerRepo := repository.NewCustomerRepository(db)
 
@@ -25,10 +30,56 @@ func UseCustomerRoute(db *sqlx.DB, app fiber.Router) {
 
 	customerConn := NewCustomerHandler(customerUse)
 
-	authR.Post("/account-register", customerConn.CreateCustomerAccount)
-	authR.Get("/getallcustomer", customerConn.GetAllCustomer)
-	authR.Get("/:customer_id", customerConn.GetCustomerById)
-	authR.Patch("/:customer_id", customerConn.UpdateCustomerById)
-	authR.Delete("/:customer_id", customerConn.DeletedCustomerByID)
+	authR.Post("/account-register",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ลูกค้า",
+				Action: "เพิ่มข้อมูล",
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+
+		customerConn.CreateCustomerAccount)
+	authR.Get("/getallcustomer",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ลูกค้า",
+				Action: "ดูข้อมูลทั้งหมด",
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		customerConn.GetAllCustomer)
+	authR.Get("/:customer_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ลูกค้า",
+				Action: "ดูข้อมูล Customer Account หมายเลข " + c.Params("customer_id"),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		customerConn.GetCustomerById)
+	authR.Patch("/:customer_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ลูกค้า",
+				Action: "อัปเดตข้อมูล Customer Account หมายเลข " + c.Params("customer_id"),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		customerConn.UpdateCustomerById)
+	authR.Delete("/:customer_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "ลูกค้า",
+				Action: "ลบข้อมูล Customer Account หมายเลข " + c.Params("customer_id"),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		customerConn.DeletedCustomerByID)
 
 }
