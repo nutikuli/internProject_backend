803d5267782021921adfd6e2dce56d547307edc6
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-05 10:24:22 +0700

    feat(product-controller): Add logging to product controller endpoints
    This commit adds logging functionality to the product controller endpoints. It introduces a new logger middleware and includes logging statements in the CreateProduct, GetProductById, GetProductsByStoreId, GetProductsByOrderId, DeleteProductById, UpdateProductById, and GetAllProducts functions. The logging enhances the tracking and monitoring of product-related actions.

--- internal/models/product/controller/http/v1/route.go
+++ internal/models/product/controller/http/v1/route.go
@@ -1,13 +1,17 @@
 package v1
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/jmoiron/sqlx"
+	_logRepo "github.com/nutikuli/internProject_backend/internal/models/logdata/repository"
 	"github.com/nutikuli/internProject_backend/internal/models/product/repository"
 	"github.com/nutikuli/internProject_backend/internal/models/product/usecase"
 	_fileRepo "github.com/nutikuli/internProject_backend/internal/services/file/repository"
 	_fileUse "github.com/nutikuli/internProject_backend/internal/services/file/usecase"
+	"github.com/nutikuli/internProject_backend/pkg/middlewares"
 )
 
 func UseProductRoute(db *sqlx.DB, app fiber.Router) {
@@ -22,13 +26,80 @@ func UseProductRoute(db *sqlx.DB, app fiber.Router) {
 	prodRepo := repository.NewproductRepository(db)
 	prodUse := usecase.NewProductUsecase(prodRepo, fileRepo, fileUse)
 
+	logRepo := _logRepo.NewLoggerRepository(db)
+	logger := middlewares.NewLogger(logRepo)
+
 	prodConn := NewProductHandler(prodUse)
 
-	prodR.Post("/create-product", prodConn.CreateProduct)
-	prodR.Get("/get-product-id/:product_id", prodConn.GetProductById)
-	prodR.Get("/get-products-by-store-id/:store_id", prodConn.GetProductsByStoreId)
-	prodR.Get("/get-products-by-order-id/:order_id", prodConn.GetProductsByOrderId)
-	prodR.Delete("/delete-product-id/:product_id", prodConn.DeleteProductById)
-	prodR.Patch("/update-product-id/:product_id", prodConn.UpdateProductById)
-	prodR.Get("/get-products", prodConn.GetAllProducts)
+	prodR.Post("/create-product",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "สินค้า",
+				Action: "เพิ่มข้อมูล",
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		prodConn.CreateProduct)
+	prodR.Get("/get-product-id/:product_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "สินค้า",
+				Action: fmt.Sprintf("ดูข้อมูล Product หมายเลข  %s", c.Params("product_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		prodConn.GetProductById)
+	prodR.Get("/get-products-by-store-id/:store_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "สินค้า",
+				Action: fmt.Sprintf("ดูข้อมูล Stores หมายเลข  %s", c.Params("store_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		prodConn.GetProductsByStoreId)
+	prodR.Get("/get-products-by-order-id/:order_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "สินค้า",
+				Action: fmt.Sprintf("ดูข้อมูล Orders หมายเลข  %s", c.Params("order_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		prodConn.GetProductsByOrderId)
+	prodR.Delete("/delete-product-id/:product_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "สินค้า",
+				Action: fmt.Sprintf("ลบข้อมูล Product หมายเลข  %s", c.Params("product_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		prodConn.DeleteProductById)
+	prodR.Patch("/update-product-id/:product_id",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "สินค้า",
+				Action: fmt.Sprintf("อัปเดตข้อมูล Product หมายเลข  %s", c.Params("product_id")),
+			}
+
+			return logger.LogRequest(c, logAction)
+
+		},
+		prodConn.UpdateProductById)
+	prodR.Get("/get-products",
+		func(c *fiber.Ctx) error {
+			logAction := &middlewares.LoggerAction{
+				Menu:   "สินค้า",
+				Action: "ดูข้อมูล",
+			}
+
+			return logger.LogRequest(c, logAction)
+		},
+		prodConn.GetAllProducts)
 }
