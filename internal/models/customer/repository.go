498c3d5ec10903a5e6563c307e95324802cc876c
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-06-06 15:32:16 +0700

    fix : customer update password

--- internal/models/customer/repository.go
+++ internal/models/customer/repository.go
@@ -11,7 +11,7 @@ type CustomerRepository interface {
 	GetAccountCustomers(ctx context.Context) ([]*entities.Customer, error)
 	GetCustomerById(ctx context.Context, customerId int64) (*entities.Customer, error)
 	CreateCustomerAccount(ctx context.Context, user *entities.CustomerRegisterReq) (*int64, error)
-	UpdateCustomerPasswordById(ctx context.Context, admindata *_accountEntities.UpdatePass) error
+	UpdateCustomerPasswordById(ctx context.Context, customerdata *_accountEntities.UpdatePass) error
 	UpdateCustomerById(ctx context.Context, userId int64, user *entities.CustomerUpdateReq) error
 	DeleteCustomerById(ctx context.Context, Id int64) error
 	// GetAllCustomer(ctx context.Context, role string) ([]*_accountEntities.Account, error)
