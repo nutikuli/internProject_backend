680110a357bcf3baee32ec9889b1a3dc25b2ce99
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-11 10:35:45 +0700

    refactor: Update OrderBankFilesRes struct and related interfaces

--- internal/models/order/dtos/order_file.go
+++ internal/models/order/dtos/order_file.go
@@ -2,14 +2,21 @@ package dtos
 
 import (
 	_bankDtos "github.com/nutikuli/internProject_backend/internal/models/bank/dtos"
+	_customerEntities "github.com/nutikuli/internProject_backend/internal/models/customer/entities"
 	_orderEntities "github.com/nutikuli/internProject_backend/internal/models/order/entities"
 	_productDtos "github.com/nutikuli/internProject_backend/internal/models/product/dtos"
 	_fileEntities "github.com/nutikuli/internProject_backend/internal/services/file/entities"
 )
 
-type OrderBanksFilesRes struct {
+type OrderBankFilesRes struct {
 	OrderData         *_orderEntities.Order          `json:"order_data"`
+	CustomerData      *_customerEntities.Customer    `json:"customer_data"`
 	FilesData         []*_fileEntities.File          `json:"files_data"`
 	BanksData         *_bankDtos.BankFileRes         `json:"bank_payment"`
 	OrdersProductData []*_productDtos.ProductFileRes `json:"product_data"`
+}
+
+type OrderBanksFilesRes struct {
+	OrderData    *_orderEntities.Order       `json:"order_data"`
+	CustomerData *_customerEntities.Customer `json:"customer_data"`
 }
--- internal/models/order/entities/order.go
+++ internal/models/order/entities/order.go
@@ -1,17 +1,17 @@
 package entities
 
 type Order struct {
-	Id           int64   `db:"id"`
-	TotalAmount  float64 `db:"totalAmount"`
-	Topic        string  `db:"topic"`
-	SumPrice     float64 `db:"sumPrice"`
-	State        string  `db:"state"`
-	DeliveryType string  `db:"deliveryType"`
-	ParcelNumber string  `db:"parcelNumber"`
-	SentDate     string  `db:"sentDate"`
-	CustomerId   int64   `db:"customerId"`
-	StoreId      int64   `db:"storeId"`
-	BankId       int64   `db:"bankId"`
-	CreatedAt    string  `db:"createdAt"`
-	UpdatedAt    string  `db:"updatedAt"`
+	Id           int64   `json:"id" db:"id"`
+	TotalAmount  float64 `json:"total_amount" db:"totalAmount"`
+	Topic        string  `json:"topic" db:"topic"`
+	SumPrice     float64 `json:"sum_price" db:"sumPrice"`
+	State        string  `json:"state" db:"state"`
+	DeliveryType string  `json:"delivery_type" db:"deliveryType"`
+	ParcelNumber string  `json:"parcel_number" db:"parcelNumber"`
+	SentDate     string  `json:"sent_date" db:"sentDate"`
+	CustomerId   int64   `json:"customer_id" db:"customerId"`
+	StoreId      int64   `json:"store_id" db:"storeId"`
+	BankId       int64   `json:"bank_id" db:"bankId"`
+	CreatedAt    string  `json:"created_at" db:"createdAt"`
+	UpdatedAt    string  `json:"updated_at" db:"updatedAt"`
 }
